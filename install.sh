fea7c95a3398cabebbf1635fc8bea047b1ce09ef
Axynera <ipstore835@gmail.com>
2026-09-12 09:50:50 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+# 1. Unduh binary playit langsung
+curl -SsL https://github.com/playit-cloud/playit-agent/releases/latest/download/playit-linux-arm64 -o /usr/local/bin/playit
+
+# 2. Beri izin eksekusi
+chmod +x /usr/local/bin/playit
+
+# 3. Jalankan playit
+playit
